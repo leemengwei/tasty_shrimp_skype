0b4069fd944bc8f808a544cf0fc67324e6284842
YettaMao <ytmao@mail.iggcas.ac.cn>
2020-02-21 01:58:22 +0800

    relentlessly re-login

--- simple.py
+++ simple.py
@@ -1,52 +1,96 @@
+#Skype 版本 8.56.0.100 2019
 from skpy import Skype
 from IPython import embed
 import os,sys
 import pandas as pd
 import tqdm
+import time
 
-def login_web_skype(username, password):
+def login_web_skype(username, password, sleep=0):
     print("Logging in as %s"%username)
+    time.sleep(sleep)
     sk = Skype(username, password) # connect to Skype
     return sk
 
-def info_to_send(info_file):
-    content = open(info_file, 'r').readlines()
+def get_template_to_send(template_file):
+    print("Getting template...")
+    content = open(template_file, 'r').readlines()
     content = ''.join(content).replace('\n', '\t\n')
     return content
 
-def output_infos():
-    for i in tqdm.tqdm(sk.contacts.contactIds):
-        f.write("******%s******"%i)
-        tmp= sk.contacts.contact(i)
-        f.write(tmp.name.first + tmp.name.last)
-        f.write(tmp.location.city + tmp.location.region + tmp.location.country)
-        f.write(tmp.birthday.ctime())
+def relentlessly_get_blob_by_id(sk, i):
+     #RELENTLESSLY:
+     try:
+         blob = sk.contacts[i]
+     except:
+         sk = None
+         blob = 'broken_tag'
+         while (sk==None) or (blob=='broken_tag'):
+             try:
+                 sk = login_web_skype(username, password, sleep=0)
+                 blob = sk.contacts[i]
+             except Exception as e:
+                 print(e)
+                 pass
+     return sk, blob
+
+def check_invalid_account(sk, all_target_people):
+    print("Checking invalid account...")
+    #check again if valid:
+    for idx,i in tqdm.tqdm(enumerate(all_target_people)):
+        print(idx)
+        sk, blob = relentlessly_get_blob_by_id(sk, i)
+        if blob is None:
+            print("Removing invalid %s"%i)
+            all_target_people.remove(i)
+        else:
+            pass
+    f = open("output/nowadays_account.txt", 'w')
+    for i in all_target_people:
+        f.write(i)
         f.write("\n")
-    for i in sk.contacts.contactIds:
-        print(sk.contacts.contact(i))
- 
-def get_all_target_people():    
+    f.close()
+    return all_target_people
+
+def get_all_target_people(sk):    
     #Get sync contacts
     print("Getting contacts...")
     sk.contacts.sync()
     all_contacts = sk.contacts.contactIds
     #others_contacts = ['maguozhi.kemen']
-    others_contacts = []
+    others_contacts = open("output/nowadays_account.txt", 'r').readlines()
+    others_contacts = ''.join(others_contacts).split('\n')[:]
     all_target_people = all_contacts + others_contacts
+    #if check valid, could be slow
+    #all_target_people = check_invalid_account(sk, all_target_people)
+    print("%s people returned"%len(all_target_people))
     return all_target_people
 
-def parse_infos(all_target_people, all_contents):
-    data = {"user_ids": all_target_people, "contents": all_contents, "SENT_FLAGS": False}
+def parse_infos(sk, all_target_people, template_contents):
+    print("Parsing infos...")
+    data = {'id':all_target_people, 'name':None, 'location':None, 'language':None, 'avatar':None, 'mood':None, 'phones':None, 'birthday':None, 'authorised':None, 'blocked':None, 'favourite':None, "contents":None, "counts":0, 'times':None, 'misc':None}
     pd_blobs = pd.DataFrame(data)
+    for each_id in tqdm.tqdm(all_target_people):
+        sk, blob = relentlessly_get_blob_by_id(sk, each_id)
+        if blob is None:continue
+        for its_attr in blob.attrs:
+            which_row = pd_blobs[pd_blobs.id==each_id].index
+            if its_attr == 'name':
+                pd_blobs.loc[which_row, its_attr] = str(eval("blob.%s"%its_attr)).split(' ')[0]
+            else:
+                pd_blobs.loc[which_row, its_attr] = str(eval("blob.%s"%its_attr))
     return pd_blobs
 
-def sent_messages(pd_blobs):
+def send_messages(sk, pd_blobs):
+    print("Sending messages...")
     for row in tqdm.tqdm(range(len(pd_blobs))):
-        this_id = pd_blobs.iloc[row].user_ids
+        this_id = pd_blobs.iloc[row].id
         this_info = pd_blobs.iloc[row].contents
-        ch = sk.contacts[this_id].chat
-        ch.sendMsg(this_info)
-        pd_blobs.SENT_FLAGS.iat[row] = True
+        sk, ch = relentlessly_get_blob_by_id(sk, this_id)
+        if ch is None:continue
+        ch = ch.chat
+        #ch.sendMsg(this_info)
+        pd_blobs.counts.iat[row] += 1
         print("Message sent for %s"%this_id)
     return
 
@@ -60,9 +104,9 @@ def misc():
     #sk.contacts # your contacts
     #sk.chats # your conversations
 
-    #ch = sk.contacts.contact(person_talk_to['id'])
+    #ch = sk.contacts.contact(person_talk_to['id'])  #by id!
     #ch = sk.contacts.search('8613120090157')
-    #ch = sk.contacts.user(person_talk_to['id'])
+    #ch = sk.contacts.user(person_talk_to['id']) #so far, same as .contact
     
     #ch = sk.chats.create([person_talk_to]) # new group conversation
     #ch = sk.contacts[person_talk_to].chat # 1-to-1 conversation
@@ -76,7 +120,7 @@ def misc():
 
 if __name__ == "__main__":
     print("Starting...")
-    info_file = "infos/content.txt"
+    template_file = "templates/content.txt"
     username = '18601156335'
     #username = 'mengxuan@bancosta.com'
     password = 'lmw196411'
@@ -86,16 +130,16 @@ if __name__ == "__main__":
     sk = login_web_skype(username, password)
 
     #Get all targets:
-    all_target_people = get_all_target_people()
+    all_target_people = get_all_target_people(sk)
  
-    #Get infos:
-    all_contents = [info_to_send(info_file)]*len(all_target_people)
+    #Get templates:
+    template_contents = [get_template_to_send(template_file)]*len(all_target_people)
 
     #Parse together:
-    pd_blobs = parse_infos(all_target_people, all_contents)
+    pd_blobs = parse_infos(sk, all_target_people, template_contents)
 
     #Send for every one:
-    sent_messages(pd_blobs)
+    send_messages(sk, pd_blobs)
  
    
     sys.exit()
